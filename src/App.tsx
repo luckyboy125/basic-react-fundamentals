25d19a9e12c9ddb9be37cbc2ffb1c69a24f82910
luckyboy125 <luckyboy.ws125@gmail.com>
2022-11-29 02:05:19 +0200

    finish left part html element

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 import ActionButton from "./component/ActionButton/ActionButton";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import { useStyles } from "./Style";
 import { ThemeContext } from "./theme/ThemeProvider";
 
 function App() {
   const classes = useStyles();
+  const [des, setDes] = useState("");
+  const buttonArray = [0, 1, 2, 3, 4];
+
   const darkTheme = "darkTheme";
   const lightTheme = "lightTheme";
   const curThemeName = localStorage.getItem("appTheme") || "darkTheme";
@@ -21,14 +25,46 @@ function App() {
     }
   };
 
+  const handleDes = (e: any) => {
+    setDes(e.target.value);
+  };
+
   return (
     <div className={classes.root}>
       <div className={classes.left}>
-        <ActionButton
-          action={handleToggle}
-          content="Toggle Theme"
-          className={classes.topBtn}
-        />
+        <div className={classes.firstPart}>
+          <ActionButton
+            action={handleToggle}
+            content="Toggle Theme"
+            className={classes.topBtn}
+          />
+        </div>
+        <div className={classes.secondPart}>
+          <textarea
+            className={classes.descriptionTextField}
+            aria-label="maximum height"
+            placeholder="Some Description"
+            onChange={(e) => {
+              handleDes(e);
+            }}
+          />
+          <ActionButton
+            action={handleToggle}
+            content="Send"
+            className={classes.sendBtn}
+          />
+        </div>
+        <div className={classes.thirdPart}>
+          {buttonArray.map((item) => {
+            return (
+              <ActionButton
+                action={handleToggle}
+                content={`Button ${item.toString()}`}
+                className={classes.increaseBtn}
+              />
+            );
+          })}
+        </div>
       </div>
       <div className={classes.right}>right part</div>
     </div>
